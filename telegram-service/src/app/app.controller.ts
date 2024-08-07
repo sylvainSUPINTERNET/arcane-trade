ef9195ade66a2738c0d4c4584d8be4d563f636c7
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-08-07 21:56:59 +0200

    [UPDATE] service telegram

--- telegram-service/src/app/app.controller.ts
+++ telegram-service/src/app/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, HttpStatus, Logger, Post, Req, Res} from '@nestjs/common';
-
+import axios from "axios";
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,14 +8,20 @@ export class AppController {
 
 
   @Post("webhook")
-  webhook(@Req() req, @Res() res) {
-    console.log("webhook : ", req.headers);
-    console.log("webhook : ", req.body);
+  async webhook(@Req() req, @Res() res) {
+    const telegramBotToken =  process.env.TELEGRAM_BOT_ACCESS_TOKEN as string;
 
     if ( req.headers["x-telegram-bot-api-secret-token"] === undefined || req.headers["x-telegram-bot-api-secret-token"] !== process.env.TELEGRAM_SECRET_TOKEN_WEBHOOK ) { 
       Logger.error("Wrong secret token webhook request is ignored. ");
       res.status(HttpStatus.FORBIDDEN).send();
     } else {
+
+
+      // Clicked on "yes" in prompt
+      if ( req.body.callback_query ) {
+        console.log("callback_query : ", req.body.callback_query);
+      }
+
       res.status(HttpStatus.OK).send();
     }
   }
--- telegram-service/src/main.ts
+++ telegram-service/src/main.ts
@@ -19,8 +19,34 @@ const bootstrapBotTelegram = async () => {
   const telegramBotToken = process.env.TELEGRAM_BOT_ACCESS_TOKEN as string;
 
   try {
+
+    const r = await axios.post(`https://api.telegram.org/bot${telegramBotToken}/deleteWebhook`);
+    console.log("Delete webhook : ", r.data);
+
     // const response = await axios.get(`https://api.telegram.org/bot${telegramBotToken}/getUpdates`);
-    // console.log(response.data.result[0].message.chat.id);
+    // console.log(response.data);
+    // const chat_id = response.data.result[0].message.chat.id;
+
+    const chat_id = process.env.TELEGRAM_CHAT_ID as string;
+    const text = 'Do you want to accept the delivery ?';
+
+    try {
+      const response = await axios.post(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+        chat_id,
+        text,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: 'Yes', callback_data: 'yes' },
+              { text: 'No', callback_data: 'no' }
+            ]
+          ]
+        }
+      });
+      console.log('Prompt sent:', response.data);
+    } catch (error) {
+      console.error('Error sending prompt:', error);
+    }
     
     const resp = await axios.post(`https://api.telegram.org/bot${telegramBotToken}/setWebhook?secret_token=${process.env.TELEGRAM_SECRET_TOKEN_WEBHOOK as string}`, {
       url: `${process.env.TELEGRAM_FORWARD_URL_WEBHOOK as string}`
