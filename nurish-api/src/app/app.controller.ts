09816cf437bcaf4fee4ea593eaf1f2417f06b4b1
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-08-15 14:44:04 +0200

    api

---
+++ nurish-api/src/app/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { AppService } from './app.service';
+import Stripe from 'stripe';
+
+@Controller(
+  "/stripe"
+)
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get("/products")
+  
+  async getProducts() {      
+    return await this.appService.getProducts();
+  }
+}
---
+++ nurish-api/src/app/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+
+@Injectable()
+export class AppService {
+  async getProducts(): Promise<Stripe.ApiList<Stripe.Product>> {
+
+    const stripe = new Stripe(process.env.STRIPE_KEY_SECRET as string)
+
+    const data = await stripe.products.list({
+      expand: ['data.prices']
+    })
+
+    return data;
+  }
+}
